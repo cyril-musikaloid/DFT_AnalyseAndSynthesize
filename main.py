758e945117ac8cc43ae7d9e30e7e13466d4386d4
cyril-muzikaloid <cyril.makloufi@gmail.com>
2022-09-27 23:52:47 +0200

    Analyse and Synthesize record

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import numpy as np
 import sounddevice as sd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 # samples per second
-smpl_rate = 8000
+smpl_rate = 2000
 
 # Record in seconds
-duration = 0.05 
+duration = 0.05
  
  # Define default sounddevice setting
 sd.default.samplerate = smpl_rate
@@ -30,7 +31,6 @@ def playEndBeep():
     sd.stop()
 
 def DFT_computeCoef(record, k):
-    N = record.size
     coef = 0
 
     for n in range(N):
@@ -39,16 +39,49 @@ def DFT_computeCoef(record, k):
     return coef
 
 def DFT_analyse(record):
-    N = record.size
-    coefVector = np.linspace(0, (int)(N/2), (int)(N/2), True, dtype=complex)
+    coefVector = np.linspace(0, (N-1), N, False, dtype=complex)
     
-    for k in range((int)(N/2)):
+    for k in range((N)):
         coefVector[k] = DFT_computeCoef(record, k)
 
     return coefVector
 
+def DFT_computeDiscretValue(vector, n):
+    dValue = 0
+
+    for k in range (N):
+        dValue += (vector[k] * np.exp(1j * ((2*np.pi)/N)*n*k))
+    
+    dValue /= N
+
+    return dValue
+
+def DFT_synthesize(vector):
+    buffer = np.linspace(0, 100*N-1, 100*N, False, dtype=complex)
+
+    for n in range (100*N):
+        buffer[n] = DFT_computeDiscretValue(vector, n)
+
+    return buffer
+
+
 def onclick(event):
-    realVector[event.xdata] = event.ydata
+    realVector[(int)(event.xdata)] = (int)(event.ydata)
+
+def onkey(event):
+    buffer = DFT_synthesize(coefVector)
+    displayPlot(np.round(np.real(buffer)))
+    displayPlot(np.imag(buffer))
+    displayPlot(record)
+    #sd.play(np.round(np.real(buffer)), loop=True, blocking=True)
+
+def displayPlot(buffer):
+    dfig, dax = plt.subplots()
+    dax.plot(buffer)
+    plt.show()
+
+def updatePlot(i):
+    plot1.set_ydata(realVector)
 
 #################################
 
@@ -57,29 +90,25 @@ playStartBeep()
  # Record the sound
 record = sd.rec(int(duration * smpl_rate), dtype='int')
 sd.wait()
- 
+N = record.size
+
+
 playEndBeep()
 
-sd.play(record, smpl_rate)
-sd.wait()
-sd.stop()
 
-fig, ax = plt.subplots()
-ax.plot(record)
-plt.show()
+sd.play(record, blocking=True)
+
+displayPlot(record)
 
 coefVector = DFT_analyse(record)
 
 realVector = np.real(np.absolute(coefVector))
 
 fig, ax = plt.subplots()
-ax.plot(realVector)
-
+plot1, =  ax.plot(realVector)
 fig.canvas.mpl_connect('button_press_event', onclick)
-plt.show()
-
-imagVector = np.imag(coefVector)
-
-fig, ax = plt.subplots()
-ax.plot(np.absolute(imagVector))
+fig.canvas.mpl_connect('key_press_event', onkey)
+ani = FuncAnimation(fig,
+                    updatePlot,
+                    interval=1)
 plt.show()
