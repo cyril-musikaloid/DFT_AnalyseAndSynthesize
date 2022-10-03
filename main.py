3966319fd0c7b5b351f42d6c963bad2a6e4a3a16
cyril-muzikaloid <cyril.makloufi@gmail.com>
2022-10-03 23:16:23 +0200

    analyseAndDisplay

--- main.py
+++ main.py
@@ -6,28 +6,28 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
 # samples per second
-smpl_rate = 8000
+_smpl_rate = 8000
 
 # Record in seconds
-duration = 0.05
+_duration = 0.05
  
  # Define default sounddevice setting
-sd.default.samplerate = smpl_rate
+sd.default.samplerate = _smpl_rate
 sd.default.channels = 1
 
 ###### Function Definition ######
 
 def playStartBeep():
-    beepArr = np.linspace(0, 1, 1 * smpl_rate, False)
+    beepArr = np.linspace(0, 1, 1 * _smpl_rate, False)
     beepArr = np.sin(440 * beepArr * 2 * np.pi) * 0.05
-    sd.play(beepArr, smpl_rate)
+    sd.play(beepArr, _smpl_rate)
     sd.wait()
     sd.stop()
 
 def playEndBeep():
-    beepArr = np.linspace(0, 1, 1 * smpl_rate, False)
+    beepArr = np.linspace(0, 1, 1 * _smpl_rate, False)
     beepArr = np.sin(380 * beepArr * 2 * np.pi) * 0.05
-    sd.play(beepArr, smpl_rate)
+    sd.play(beepArr, _smpl_rate)
     sd.wait()
     sd.stop()
 
@@ -124,60 +124,6 @@ def reworkRecord(buffer):
     pos = findPatternPosition(ascending, buffer, buffer[buffer.size-1])
     return buffer[pos : ]
 
-#################################
-
-##### Computation Function ######
-
-def _2_WkN(N, k):
-    N_vec = np.arange(1, N)
-    k_vec = N_vec / 2
-    result = np.exp(-1j*((2*np.pi)/N_vec)*k_vec)
-    displayPlot(result)
-    
-    result = np.exp(-1j*((2*np.pi)/N_vec)+N_vec)
-    displayPlot(result)
-
-def _3():
-    f = np.sqrt(2)
-    n = np.arange(-50, 50)
-    signal = np.exp(-1j*f*n) + np.exp(+1j*f*n)
-    displayPlot(signal)
-
-    signal = np.sin(n)
-    displayPlot(signal)
-
-    t = np.linspace(-1000, 1000, 20000)
-    signal = t - np.floor(t)
-    displayPlot(signal)
-
-    f = 5
-    phi = 90
-    signal = np.cos(2*np.pi*f*t + phi)
-    displayPlot(signal)
-
-def _4():
-    N = 64
-    n = np.arange(N)
-    signal = np.power(-1, n)
-    DFT_analyseAndDisplay(signal)
-
-    L = N/2
-    phi = 90
-    signal = np.cos(((2*np.pi)/N)*L*n + phi)
-
-    for k in range(N):
-        coef = DFT_computeCoef(signal, k)
-        if coef == ((N/2)*np.exp(1j*phi)) and (not k == L):
-            print("error\n")
-        elif coef == 0 and k == L:
-            print("error\n")
-        
-    DFTed_Signal = DFT_analyse(signal)
-    displayPlot(np.real(DFTed_Signal))
-    print(((N/2)*np.exp(1j*phi)))
-    print(DFT_computeCoef(signal, L))
-    print(((N/2)*np.exp(-1j*phi)))
-    print(DFT_computeCoef(signal, N-L))
 
 def genSinWave(N, f = 1, a = 1, phi = 0):
     n = np.arange(N)
@@ -189,41 +135,21 @@ def DFT_analyseAndDisplay(signal):
     DFTed_Signal = DFT_analyse(signal)
     displayAllPlot(np.real(DFTed_Signal), np.imag(DFTed_Signal), np.real(signal), np.imag(signal))
 
-def _6():
-    phi = 0
-    f = 0.4
-    N = 64
-    n = np.arange(N)
-    a = 2
+def record(duration = _duration, sampleRate = _smpl_rate):
+    playStartBeep()
 
-    signal = genSinWave(N, f, a, phi)
-    DFT_analyseAndDisplay(signal)
+    record=sd.rec(int(duration * sampleRate))
+    sd.wait()
 
-    signal = genSinWave(N, 0.8, 0.5, -90)
-    DFT_analyseAndDisplay(signal)
-
-def _7():
-    N = 64
-    L = 10
-    M = N
-    n = np.arange(N)
-    signal = np.cos(2*np.pi*(L/M)*n)
-    DFT_analyseAndDisplay(signal)
-
-    n = np.roll(n,12)
-    signal = np.cos(2*np.pi*(L/M)*n)
-    DFT_analyseAndDisplay(signal)
-
-def _8():
-    signal = genSinWave(65, 0.5, phi=31)
-    DFT_analyseAndDisplay(signal)
-
-def _9():
-    N = 65
-    n = np.arange(N)
-    signal = -np.power(-1, n)
-    DFT_analyseAndDisplay(signal)
+    playEndBeep()
+    return record
 
 #################################
 
-_7()
+##### Computation Function ######
+
+#################################
+
+record = record(duration=2)
+
+DFT_analyseAndDisplay(record)
