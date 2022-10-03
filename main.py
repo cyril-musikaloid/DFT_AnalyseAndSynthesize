f05f6d862629a9d36e09eb10f04ab5869c251ccb
cyril-muzikaloid <cyril.makloufi@gmail.com>
2022-10-03 19:57:45 +0200

    tools for third week quizz

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from signal import signal
 import numpy as np
 import sounddevice as sd
 import matplotlib as mpl
@@ -31,14 +32,15 @@ def playEndBeep():
     sd.stop()
 
 def DFT_computeCoef(record, k):
-    coef = 0
-
+    coef = 0+0j
+    N = record.size
     for n in range(N):
         coef += record[n] * np.exp(-1j*((2*np.pi)/N)*n*k)
     
     return coef
 
 def DFT_analyse(record):
+    N = record.size
     coefVector = np.linspace(0, (N-1), N, False, dtype=complex)
     
     for k in range((N)):
@@ -75,20 +77,24 @@ def onkey(event):
     displayAllPlot(realBuffer, imagBuffer, record)
     sd.play(np.int32(np.round(np.real(buffer))), loop=True, blocking=False)
 
-def displayAllPlot(realBuffer, imagBuffer, record):
+def displayAllPlot(realBuffer, imagBuffer, record_real, record_imag):
     aFig, axd = plt.subplot_mosaic([['freq'],
                                     ['.'],
                                     ['phase'],
                                     ['.'],
-                                    ['record']])
+                                    ['record_real'],
+                                    ["."],
+                                    ["record_imag"]])
 
     axd['freq'].set_title('frequencies')
     axd['phase'].set_title('phase')
-    axd['record'].set_title('record')
+    axd['record_real'].set_title('record real')
+    axd['record_imag'].set_title('record imag')
 
     axd['freq'].plot(realBuffer)
     axd['phase'].plot(imagBuffer)
-    axd['record'].plot(record)
+    axd['record_real'].plot(record_real)
+    axd['record_imag'].plot(record_imag)
 
     plt.show()
 
@@ -120,35 +126,104 @@ def reworkRecord(buffer):
 
 #################################
 
-playStartBeep()
+##### Computation Function ######
 
- # Record the sound
-record = sd.rec(int(duration * smpl_rate), dtype='int')
-sd.wait()
+def _2_WkN(N, k):
+    N_vec = np.arange(1, N)
+    k_vec = N_vec / 2
+    result = np.exp(-1j*((2*np.pi)/N_vec)*k_vec)
+    displayPlot(result)
+    
+    result = np.exp(-1j*((2*np.pi)/N_vec)+N_vec)
+    displayPlot(result)
 
+def _3():
+    f = np.sqrt(2)
+    n = np.arange(-50, 50)
+    signal = np.exp(-1j*f*n) + np.exp(+1j*f*n)
+    displayPlot(signal)
 
-playEndBeep()
+    signal = np.sin(n)
+    displayPlot(signal)
 
-record = reworkRecord(record)
-N = record.size
+    t = np.linspace(-1000, 1000, 20000)
+    signal = t - np.floor(t)
+    displayPlot(signal)
 
+    f = 5
+    phi = 90
+    signal = np.cos(2*np.pi*f*t + phi)
+    displayPlot(signal)
 
+def _4():
+    N = 64
+    n = np.arange(N)
+    signal = np.power(-1, n)
+    DFT_analyseAndDisplay(signal)
 
+    L = N/2
+    phi = 90
+    signal = np.cos(((2*np.pi)/N)*L*n + phi)
 
-sd.play(record, blocking=True)
+    for k in range(N):
+        coef = DFT_computeCoef(signal, k)
+        if coef == ((N/2)*np.exp(1j*phi)) and (not k == L):
+            print("error\n")
+        elif coef == 0 and k == L:
+            print("error\n")
+        
+    DFTed_Signal = DFT_analyse(signal)
+    displayPlot(np.real(DFTed_Signal))
+    print(((N/2)*np.exp(1j*phi)))
+    print(DFT_computeCoef(signal, L))
+    print(((N/2)*np.exp(-1j*phi)))
+    print(DFT_computeCoef(signal, N-L))
 
-displayPlot(record)
+def genSinWave(N, f = 1, a = 1, phi = 0):
+    n = np.arange(N)
+    signal = a * np.cos(n * f + (phi*np.pi/180))
+    #signal = a*np.exp(1j*(f*n+phi)) provoks a dephased imaginary part
+    return signal
 
-coefVector = DFT_analyse(record)
+def DFT_analyseAndDisplay(signal):
+    DFTed_Signal = DFT_analyse(signal)
+    displayAllPlot(np.real(DFTed_Signal), np.imag(DFTed_Signal), np.real(signal), np.imag(signal))
 
-realVector = np.real(coefVector)
-imagVector = np.imag(coefVector)
+def _6():
+    phi = 0
+    f = 0.4
+    N = 64
+    n = np.arange(N)
+    a = 2
 
-fig, ax = plt.subplots()
-plot1, =  ax.plot(realVector)
-fig.canvas.mpl_connect('button_press_event', onclick)
-fig.canvas.mpl_connect('key_press_event', onkey)
-ani = FuncAnimation(fig,
-                    updatePlot,
-                    interval=1)
-plt.show()
+    signal = genSinWave(N, f, a, phi)
+    DFT_analyseAndDisplay(signal)
+
+    signal = genSinWave(N, 0.8, 0.5, -90)
+    DFT_analyseAndDisplay(signal)
+
+def _7():
+    N = 64
+    L = 10
+    M = N
+    n = np.arange(N)
+    signal = np.cos(2*np.pi*(L/M)*n)
+    DFT_analyseAndDisplay(signal)
+
+    n = np.roll(n,12)
+    signal = np.cos(2*np.pi*(L/M)*n)
+    DFT_analyseAndDisplay(signal)
+
+def _8():
+    signal = genSinWave(65, 0.5, phi=31)
+    DFT_analyseAndDisplay(signal)
+
+def _9():
+    N = 65
+    n = np.arange(N)
+    signal = -np.power(-1, n)
+    DFT_analyseAndDisplay(signal)
+
+#################################
+
+_7()
